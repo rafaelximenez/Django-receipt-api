0ad1f74f9c9b0b96a835ba8cd509c13eff639f0a
Rafael Ximenez <rximenez97@gmail.com>
2021-09-27 22:32:49 -0300

    adding CRUD

--- api/rxmz/urls.py
+++ api/rxmz/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from rxmz import views 
  
 urlpatterns = [ 
-    url(r'^api/recibos$', views.index),
+    url(r'^recibos$', views.index),
+    url(r'^buscar/recibos/(?P<pk>[0-9]+)$', views.get),
+    url(r'^criar/recibos', views.create),
+    url(r'^alterar/recibos/(?P<pk>[0-9]+)$', views.change),
+    url(r'^deletar/recibos/(?P<pk>[0-9]+)$', views.delete),
 ]
--- api/rxmz/views.py
+++ api/rxmz/views.py
@@ -15,3 +15,40 @@ def index(request):
         
     recibos_serializer = RecibosSerializer(recibos, many=True)
     return JsonResponse(recibos_serializer.data, safe=False)
+
+@api_view(['GET'])
+def get(request, pk):
+    recibos = Recibos.objects.get(pk=pk)
+    recibos_serializer = RecibosSerializer(recibos) 
+    return JsonResponse(recibos_serializer.data)
+
+@api_view(['POST'])
+def create(request):
+    if request.method == 'POST':
+        request_body = JSONParser().parse(request)
+        recibos_serializer = RecibosSerializer(data=request_body) 
+        if recibos_serializer.is_valid():
+            recibos_serializer.save()
+            return JsonResponse(recibos_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return "Método não suportado", 405
+
+@api_view(['PUT'])
+def change(request, pk):
+    if request.method == 'PUT':
+        recibo = Recibos.objects.get(pk=pk)
+        request_body = JSONParser().parse(request)
+        recibos_serializer = RecibosSerializer(recibo, data=request_body)
+        if recibos_serializer.is_valid():
+            recibos_serializer.save()
+            return JsonResponse(recibos_serializer.data)
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return "Método não suportado", 405
+
+@api_view(['DELETE'])
+def delete(request, pk):
+    if request.method == 'DELETE':
+        recibo = Recibos.objects.get(pk=pk)
+        recibo.delete()
+        return JsonResponse({'message': 'O recibo foi excluído com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
+    return "Método não suportado", 405
